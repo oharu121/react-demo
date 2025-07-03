348276fc5a9d44ebc8370c11142c9532473dd75c
oharu121 <jefflin1201@gmail.com>
2025-07-03 10:21:37 +0900

    make each component as page

---
+++ app/scroll-example/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import ScrollExample from "@/components/ScrollExample";
+
+export default function Page() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>ScrollExample Demo</h1>
+      <ScrollExample />
+    </main>
+  );
+}
